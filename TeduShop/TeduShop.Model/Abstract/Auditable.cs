6d254cd07e9c8881644d01997dd1ee543e6730bf
Hoang Long <ndhlong1994@gmail.com>
2017-01-07 14:12:54 +0700

    add model entities for project

---
+++ TeduShop/TeduShop.Model/Abstract/Auditable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TeduShop.Model.Abstract
+{
+    public abstract class Auditable : IAuditable
+    {
+        [MaxLength(256)]
+        public String MetaKeyword { get; set; }
+
+        [MaxLength(256)]
+        public String MetaDescription { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+
+        [MaxLength(256)]
+        public String CreateBy { get; set; }
+
+        public DateTime? UpdateDate { get; set; }
+
+        [MaxLength(256)]
+        public String UpdateBy { get; set; }
+
+        public bool Status { get; set; }
+
+    }
+}
---
+++ TeduShop/TeduShop.Model/Abstract/IAuditable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TeduShop.Model.Abstract
+{
+    public interface IAuditable
+    {
+        String MetaKeyword { get; set; }
+
+        String MetaDescription { get; set; }
+
+        DateTime? CreatedDate { get; set; }
+
+        String CreateBy { get; set; }
+
+        DateTime? UpdateDate { get; set; }
+
+        String UpdateBy { get; set; }
+
+        bool Status { get; set; }
+
+    }
+}
